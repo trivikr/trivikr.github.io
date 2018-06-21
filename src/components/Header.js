2a734c9cb94074a5b488535ee92fb320d4a54cfb
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-06-20 20:30:43 -0700

    Add description

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,11 @@ class Header extends React.Component {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <a href="https://twitter.com/trivikram" className="image avatar"><img src={avatar} alt="" /></a>
+                    <h1><strong>Trivikram Kamat</strong></h1>
+                    <h4>Software Development Engineer</h4>
+                    <div><a href="https://github.com/nodejs/node">Node.js</a> core collaborator</div>
+                    <div>(he/him)</div>
                 </div>
                 <Footer />
             </header>
