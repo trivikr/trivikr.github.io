af7d567e884232e72dfd100fd42ec3b53bd13175
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-06-20 18:13:42 -0700

    Created basic website with Gatsby Starter

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/trivikr/workspace/trivikr.github.io/src/layouts/index.js"
+  import data from "/Users/trivikr/workspace/trivikr.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
