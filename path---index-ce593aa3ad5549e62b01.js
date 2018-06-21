600922f331e5c1ccf1fe0fc7e3bdf184a1dfa74a
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-06-20 20:01:08 -0700

    Revert "Updates"
    This reverts commit 5d0789c0ac9b236ab5369404c638dfb920386292.

--- path---index-ce593aa3ad5549e62b01.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x81b8806e4260],{413:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Gatsby Starter - Strata by HTML5 UP",description:"A Gatsby.js Starter based on Strata by HTML5 UP"}}},pathContext:{}}}});
-//# sourceMappingURL=path---index-ce593aa3ad5549e62b01.js.map
