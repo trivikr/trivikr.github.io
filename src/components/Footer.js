8200a6c98958bc8b2c66dcc102a7d1d1dc0cb7d0
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2018-06-20 20:40:21 -0700

    Add Social Media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,11 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://twitter.com/trivikram" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://github.com/trivikr" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/trivikramkamat/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                        <li><a href="https://www.youtube.com/user/trivikramkamat" className="icon fa-youtube"><span className="label">YouTube</span></a></li>
+                        <li><a href="https://medium.com/@trivikram" className="icon fa-medium"><span className="label">Medium</span></a></li>
                     </ul>
                     <ul className="copyright">
                         <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
